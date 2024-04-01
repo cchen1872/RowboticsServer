6729dd7cff7ace78315551c2b4b0cdcdb002967a
Calvin CHen <cchen1872@gmail.com>
2024-03-31 20:14:56 -0400

    event source set up

---
+++ data/sse.py
@@ -0,0 +1,5 @@
+def format_sse(data: str, event=None) -> str:
+    msg = f'data: {data}\n\n'
+    if event is not None:
+        msg = f'event: {event}\n{msg}'
+    return msg
--- server/endpoints.py
+++ server/endpoints.py
@@ -3,7 +3,7 @@ This is the file containing all of the endpoints for our flask app.
 The endpoint called `endpoints` will return all available endpoints.
 """
 
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_restx import Resource, Api, fields
 from flask_cors import CORS
 from http.client import (
@@ -13,21 +13,61 @@ from http.client import (
     NO_CONTENT,
     BAD_REQUEST
 )
+from os import getpid, kill
+from signal import SIGTERM
+from data.MessageAnnouncer import MessageAnnouncer
+from data.sse import format_sse
+
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-@api.route('/hello')
-class HelloWorld(Resource):
+announcer = MessageAnnouncer()
+listening_probe = False
+
+@api.route('/listen')
+class Listener(Resource):
     """
     The purpose of the HelloWorld class is to have a simple test to see if the
     app is working at all.
     """
     def get(self) -> dict:
-        """
-        A trivial endpoint to see if the server is running.
-        It just answers with "hello world."
-        """
-        return {'hello': 'world'}
+        global listening_probe
+        def stream():
+            # global listening_probe
+            messages = announcer.listen()  # returns a queue.Queue
+            while listening_probe:
+                msg = messages.get()  # blocks until a new message arrives
+                yield msg
+            return None
+        
+        listening_probe = True
+        return Response(stream(), mimetype='text/event-stream')
+
+
+@api.route('/end_stream')
+class EndStream(Resource):
+    """
+    The purpose of the HelloWorld class is to have a simple test to see if the
+    app is working at all.
+    """
+    def patch(self):
+        global listening_probe
+        listening_probe = False
+        msg = format_sse(data='finished')
+        announcer.announce(msg=msg)
+        
+
+
+@api.route('/ping')
+class Ping(Resource):
+    """
+    The purpose of the HelloWorld class is to have a simple test to see if the
+    app is working at all.
+    """
+    def patch(self):
+        msg = format_sse(data='pong')
+        announcer.announce(msg=msg)
+        return {}, 200
 
